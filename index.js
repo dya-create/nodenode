2834d66b224a8a2f45db88281bc64bf332f7c739
dya-create <78065221+dya-create@users.noreply.github.com>
2021-10-07 09:54:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 
-
-http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('index.html', null, function(error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
+http.createServer(function(req, res){
+    fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
     });
-    const port = process.env.PORT || 1336;
-}).listen(port);
+}).listen(8000);
 
 
 
