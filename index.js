55d21d6af077ecfc6dcdba98763ffc4d6b4c01fa
dya-create <78065221+dya-create@users.noreply.github.com>
2021-10-07 10:17:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
 
-http.createServer(function(req, res){
-    fs.readFile('index.html',function (err, data){
-        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-        res.write(data);
-        res.end();
-    });
-}).listen(8000);
+    console.log(req.url, req.method);
+//set header content type
+res.setHeader('Content-Type', 'text/html');
+// res.write('Welcome to ckmobile');
+fs.readFile('index.html', (err, data) => {
+if (err) {
+    console.log(err)
+    res.end()
+} else {
+    res.end(data);
+}
+})
+})
+const port = process.env.PORT || 3000
+server.listen(port);
+console.log('listening for request on port 3000');
 
 
 
